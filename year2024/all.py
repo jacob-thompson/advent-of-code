bb598a8524797d900f45da933751db027d894916
Jacob Thompson <jacobalthompson@gmail.com>
2024-12-04 16:57:17 -0800

    error checking
    user must run `all.py` from `year` directory

--- year2024/all.py
+++ year2024/all.py
@@ -4,15 +4,20 @@ from subprocess import run
 SEPARATOR = "---------------"
 SOLUTION = "python3 solution.py"
 
+def run_solutions(directory):
+    for _, folders, _ in os.walk(directory):
+        for index, folder in enumerate(folders):
+            os.chdir(f"{directory}/{folder}")
+            print(f"{SEPARATOR} day{index + 1:02} {SEPARATOR}")
+            run(SOLUTION.split(" "))
+            os.chdir(f"{directory}")
+
 def main():
     current_directory = os.getcwd()
-
-    for _, folders, _ in os.walk(current_directory):
-        for index, folder in enumerate(folders):
-            print(f"{SEPARATOR} day{index + 1:02} {SEPARATOR}")
-            os.chdir(f"{current_directory}/{folder}")
-            run(SOLUTION.split(" "))
-            os.chdir(f"{current_directory}")
+    try:
+        run_solutions(current_directory)
+    except FileNotFoundError:
+        print("ERROR LOADING SOLUTION SCRIPTS")
 
 if __name__ == "__main__":
     main()
