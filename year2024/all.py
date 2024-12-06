d78a01d1ee17c495d97d27658318885194384273
Jacob Thompson <jacobalthompson@gmail.com>
2024-12-06 01:37:45 -0800

    fix all.py executing scripts in unintended order

--- year2024/all.py
+++ year2024/all.py
@@ -6,18 +6,14 @@ SOLUTION = "python3 solution.py"
 
 def run_solutions(directory):
     for _, folders, _ in os.walk(directory):
-        for index, folder in enumerate(folders):
+        for index, folder in enumerate(sorted(folders)):
             os.chdir(f"{directory}/{folder}")
             print(f"{SEPARATOR} day{index + 1:02} {SEPARATOR}")
             run(SOLUTION.split(" "))
             os.chdir(f"{directory}")
 
-def main():
-    current_directory = os.getcwd()
-    try:
-        run_solutions(current_directory)
-    except FileNotFoundError:
-        print("ERROR LOADING SOLUTION SCRIPTS")
-
-if __name__ == "__main__":
-    main()
+current_directory = os.getcwd()
+try:
+    run_solutions(current_directory)
+except FileNotFoundError:
+    print("ERROR LOADING SOLUTION SCRIPTS")
